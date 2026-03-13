ed9142ac7ddaefcb281be7d938c254ba26d26378
Alexandria <154698469+Alexandria007@users.noreply.github.com>
2026-03-14 02:55:26 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react'
 import './index.css'
 import './App.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AptosWalletAdapterProvider
+      autoConnect={false}
+      dappConfig={{ network: 'testnet' as any }}
+      onError={(error) => console.error('Wallet error:', error)}
+    >
+      <App />
+    </AptosWalletAdapterProvider>
   </StrictMode>,
 )
