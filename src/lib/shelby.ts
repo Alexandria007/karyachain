cfff15fb5e2ed8dd039eb8f5be119028aeee5bd0
Alexandria <154698469+Alexandria007@users.noreply.github.com>
2026-03-19 14:58:04 +0700

    Add files via upload

---
+++ src/lib/shelby.ts
@@ -0,0 +1,8 @@
+import { ShelbyClient } from '@shelby-protocol/sdk/browser'
+import { Network } from '@aptos-labs/ts-sdk'
+
+export const shelbyClient = new ShelbyClient({
+  network: Network.TESTNET,
+  apiKey: import.meta.env.VITE_APTOS_API_KEY,
+  indexerApiKey: import.meta.env.VITE_APTOS_API_KEY,
+})
